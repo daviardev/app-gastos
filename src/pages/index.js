c863b0d400c5f7289a6ceac2a913d1616517d6b1
daviardev <jerson.silva99@hotmail.com>
2023-05-07 17:40:22 -0500

    Move to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 
 import Header from 'components/Header'
-import Expenses from 'components/Expenses'
+import ExpenseCategory from 'components/ExpenseCategory'
+
+import { currencyFormatter } from 'utils/currencyFormatter'
 
 export default function Home () {
   return (
@@ -15,7 +17,24 @@ export default function Home () {
 
       <Header />
 
-      <Expenses />
+      <main className='container max-w-2xl px-6 mx-auto'>
+        <section className='py-2'>
+          <small className='text-gray-400 text-md'>Mi saldo</small>
+          <h2 className='text-4xl font-bold'>{currencyFormatter(200000)}</h2>
+        </section>
+
+        <section className='flex items-center gap-2 py-3'>
+          <button className='btn btn-primary'>+ Añadir gastos</button>
+          <button className='btn btn-primary-outline'>+ Añadir ingreso</button>
+        </section>
+
+        <section className='py-6'>
+          <h3 className='text-2xl'>Mis gastos</h3>
+          <div className='flex flex-col gap-4 mt-6'>
+            <ExpenseCategory />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
