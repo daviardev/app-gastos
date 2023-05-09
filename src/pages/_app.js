de54e180818ee3222e3f7d8fbf6f4e34e69ec387
daviardev <jerson.silva99@hotmail.com>
2023-05-09 15:47:07 -0500

    Envolve context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import 'styles/globals.css'
 
+import { AppContext } from 'context/AppContext'
+
 export default function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppContext children={children}>
+      <Component { ...pageProps } />
+    </AppContext>
+  )
 }
