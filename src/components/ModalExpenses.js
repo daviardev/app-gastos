e57d988a59785f9b4b35240af63505778443183e
daviardev <jerson.silva99@hotmail.com>
2023-05-14 21:39:56 -0500

    Remove conditional for show categories

--- src/components/ModalExpenses.js
+++ src/components/ModalExpenses.js
@@ -94,76 +94,75 @@ export default function ModalExpenses ({ show, onClose }) {
               className={`input ${loading && 'opacity-60'}`}
               placeholder='Ingrese la cantidad que va a gastar'
             />
-            {inputExponses > 0 && (
-              <div className='flex flex-col gap-4 mt-6'>
+            <div className='flex flex-col gap-4 mt-6'>
+              <div className='flex items-center justify-between'>
+                <h3 className='text-1xl'>Seleccione una de las categorías creadas</h3>
+                <button className='btn btn-primary-outline' onClick={() => setShowAddExpense(true)}>
+                  Crear nueva categoría
+                </button>
+              </div>
+              {showAddExpense && (
                 <div className='flex items-center justify-between'>
-                  <h3 className='text-1xl'>Seleccione una de las categorías creadas</h3>
-                  <button className='btn btn-primary-outline' onClick={() => setShowAddExpense(true)}>
-                    Crear nueva categoría
+                  <input
+                    type='text'
+                    value={inputTitle}
+                    required
+                    onChange={e => setInputTitle(e.target.value)}
+                    placeholder='Ingrese título'
+                    className={`${loading && 'opacity-60'}`}
+                  />
+
+                  <label>Escoja color</label>
+                  <input
+                    type='color'
+                    ref={colorRef}
+                    className={`w-24 h-10 input ${loading && 'opacity-60'}`}
+                  />
+                  <button
+                    onClick={addCategoryHandler}
+                    className={`btn btn-primary-outline disabled:opacity-40 ${loading && 'opacity-60'}`}
+                    disabled={!inputTitle.trim()}
+                  >
+                    Crear categoría
+                  </button>
+                  <button
+                    onClick={() => {
+                      setShowAddExpense(false)
+                    }}
+                    className='btn btn-danger'
+                  >
+                    Cancelar
                   </button>
                 </div>
-                {showAddExpense && (
-                  <div className='flex items-center justify-between'>
-                    <input
-                      type='text'
-                      value={inputTitle}
-                      required
-                      onChange={e => setInputTitle(e.target.value)}
-                      placeholder='Ingrese título'
-                      className={`${loading && 'opacity-60'}`}
-                    />
-
-                    <label>Escoja color</label>
-                    <input
-                      type='color'
-                      ref={colorRef}
-                      className={`w-24 h-10 input ${loading && 'opacity-60'}`}
-                    />
-                    <button
-                      onClick={addCategoryHandler}
-                      className={`btn btn-primary-outline disabled:opacity-40 ${loading && 'opacity-60'}`}
-                      disabled={!inputTitle.trim()}
-                    >
-                      Crear categoría
-                    </button>
-                    <button
-                      onClick={() => {
-                        setShowAddExpense(false)
-                      }}
-                      className='btn btn-danger'
-                    >
-                      Cancelar
-                    </button>
-                  </div>
-                )}
-                <div className='input-group'>
-                  <div className='flex flex-col gap-4 h-[195px] overflow-auto history-scroll'>
-                    {expenses.map(index => {
-                      return (
-                        <button
-                          type='button'
-                          key={index.id}
-                          onClick={() => setSelectCategory(index.id)}
+              )}
+              <div className='input-group'>
+                <div className='flex flex-col gap-4 h-[195px] overflow-auto history-scroll'>
+                  {expenses.map(index => {
+                    return (
+                      <button
+                        type='button'
+                        key={index.id}
+                        onClick={() => setSelectCategory(index.id)}
+                      >
+                        <div
+                          style={{ boxShadow: index.id === selectCategory ? '1px 1px 4px' : 'none' }}
+                          className='flex items-center justify-between px-2 py-2 bg-slate-700 rounded-full'
                         >
-                          <div
-                            style={{ boxShadow: index.id === selectCategory ? '1px 1px 4px' : 'none' }}
-                            className='flex items-center justify-between px-2 py-2 bg-slate-700 rounded-full'
-                          >
-                            <div className='flex items-center gap-2'>
-                              <div
-                                style={{ backgroundColor: index.color }}
-                                className='w-[25px] h-[25px] rounded-full'
-                              />
-                              <h4 className='capitalize'>{index.title}</h4>
-                            </div>
+                          <div className='flex items-center gap-2'>
+                            <div
+                              style={{ backgroundColor: index.color }}
+                              className='w-[25px] h-[25px] rounded-full'
+                            />
+                            <h4 className='capitalize'>{index.title}</h4>
                           </div>
-                        </button>
-                      )
-                    })}
-                  </div>
+                        </div>
+                      </button>
+                    )
+                  })}
                 </div>
               </div>
-            )}
+            </div>
+
             {inputExponses > 0 && selectCategory && (
               <div className='flex justify-center'>
                 <button
