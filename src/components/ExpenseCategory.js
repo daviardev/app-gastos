c44e1e185836708f9889db11bf10e39115dc6682
daviardev <jerson.silva99@hotmail.com>
2023-05-14 23:23:22 -0500

    Add modal for show expenses and remove expense

--- src/components/ExpenseCategory.js
+++ src/components/ExpenseCategory.js
@@ -1,15 +1,30 @@
+import { useState } from 'react'
+
 import { currencyFormatter } from 'utils/currencyFormatter'
 
-export default function ExpenseCategory ({ color, title, total }) {
+import ViewExpenseModal from './ViewExpenseModal'
+
+export default function ExpenseCategory ({ expense }) {
+  const [showViewExpenseModal, setShowViewExpenseModal] = useState(false)
   return (
-    <button>
-      <div className='flex items-center justify-between px-4 py-4 bg-red-500 rounded-3xl'>
-        <div className='flex items-center gap-2'>
-          <div className='w-[25px] h-[25px] rounded-full' style={{ backgroundColor: color }} />
-          <h4 className='capitalize'>{title}</h4>
+    <>
+      <ViewExpenseModal
+        onClose={setShowViewExpenseModal}
+        show={showViewExpenseModal}
+        expense={expense}
+      />
+      <button
+        onClick={() => setShowViewExpenseModal(true)}
+      >
+        <div className='flex items-center justify-between px-4 py-4 bg-red-500 rounded-3xl'>
+          <div className='flex items-center gap-2'>
+            <div className='w-[25px] h-[25px] rounded-full' style={{ backgroundColor: expense.color }} />
+            <h4 className='capitalize'>{expense.title}</h4>
+          </div>
+          <p>{currencyFormatter(expense.total)}</p>
         </div>
-        <p>{currencyFormatter(total)}</p>
-      </div>
-    </button>
+      </button>
+
+    </>
   )
 }
---
+++ src/components/ViewExpenseModal.js
@@ -0,0 +1,68 @@
+import { useContext } from 'react'
+
+import { FaRegTrashAlt } from 'react-icons/fa'
+
+import { AppContext } from 'context/AppContext'
+import { currencyFormatter } from 'utils/currencyFormatter'
+
+import Modal from './Modal'
+
+export default function ViewExpenseModal ({ show, onClose, expense }) {
+  const { deleteExpenseItem } = useContext(AppContext)
+
+  const deleteExpenseItemHandler = async item => {
+    try {
+      const updatedItems = expense.items.filter(index => index.id !== item.id)
+
+      const updatedExpense = {
+        items: [...updatedItems],
+        total: expense.total - item.amount
+      }
+
+      await deleteExpenseItem(updatedExpense, expense.id)
+    } catch (err) {
+      console.error(err)
+      throw err
+    }
+  }
+  return (
+    <Modal
+      show={show}
+      onClose={onClose}
+    >
+      <div className='flex items-center justify-between'>
+        <h2 className='text-4xl capitalize'>{expense.title}</h2>
+        <button className='btn btn-danger'>Eliminar</button>
+      </div>
+      <br />
+      <hr />
+
+      <div>
+        <h3 className='my-4 text-2xl'>Historial de gastos</h3>
+        {expense.items.map(item => {
+          return (
+            <div
+              key={item.id}
+              className='flex items-center justify-between'
+            >
+              <small>
+                {item.createdAt.toMillis
+                  ? new Date(item.createdAt.toMillis()).toISOString()
+                  : item.createdAt.toISOString()}
+              </small>
+              <p className='flex items-center gap-2'>
+                {currencyFormatter(item.amount)}
+                <button
+                  onClick={() => deleteExpenseItemHandler(item)}
+                  className='hover:text-red-500'
+                >
+                  <FaRegTrashAlt />
+                </button>
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    </Modal>
+  )
+}
