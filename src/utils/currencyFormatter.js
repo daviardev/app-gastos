38cb3c7d0c2931427b7fae07a4f7e6557475b2e9
daviardev <jerson.silva99@hotmail.com>
2023-05-19 15:38:13 -0500

    Fix format

--- src/utils/currencyFormatter.js
+++ src/utils/currencyFormatter.js
@@ -1,4 +1,9 @@
 export const currencyFormatter = amount => {
+  if (amount >= 10 ** 11) {
+    const formattedAmount = amount.toExponential(2)
+    return formattedAmount.replace(/e\+/, 'x10^')
+  }
+
   const formatter = Intl.NumberFormat('es-CO', {
     currency: 'COP',
     style: 'currency'
